1927caaff1b9dfea135489a3474dd1ab8ecc59aa
Roger Mabillard <roger.mabillard@gmail.com>
2011-09-18 04:03:12 -0400

    Fixed: slow full size image loading - used GCD to process in a new queue

--- FGallery/Classes/FGalleryViewController.m
+++ FGallery/Classes/FGalleryViewController.m
@@ -833,13 +833,15 @@
 - (void)loadFullsizeImageWithIndex:(NSUInteger)index
 {
 //	NSLog(@"loadFullsizeImageWithIndex: %i", index );
-	
-	FGalleryPhoto *photo = [_photoLoaders objectForKey:[NSString stringWithFormat:@"%i", index]];
-	
-	if( photo == nil )
-		photo = [self createGalleryPhotoForIndex:index];
-	
-	[photo loadFullsize];
+#warning Do this using GCD
+    dispatch_queue_t loadQueue = dispatch_queue_create("image loading queue", NULL);
+    dispatch_async(loadQueue, ^{
+        FGalleryPhoto *photo = [_photoLoaders objectForKey:[NSString stringWithFormat:@"%i", index]];
+        if( photo == nil )
+            photo = [self createGalleryPhotoForIndex:index];
+        [photo loadFullsize];
+    });
+    dispatch_release(loadQueue);
 }
 
 
